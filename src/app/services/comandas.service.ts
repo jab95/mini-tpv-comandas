4f4289cb38a3c8f4673ed3f8aedee22be2561085
jose <josejab95@gmail.com>
2022-11-26 16:12:40 +0100

    cambia url de back

--- src/app/services/comandas.service.ts
+++ src/app/services/comandas.service.ts
@@ -10,7 +10,7 @@ export class ComandasService {
   constructor(private http:HttpClient) { }
 
   public findComandas(page:any,limit:any){
-    let url = `http://localhost:3100/comandas/getAll?page=${page}&limit=${limit}`
+    let url = `https://mini-tpv-backend-production.up.railway.app:3100/comandas/getAll?page=${page}&limit=${limit}`
     return this.http.get<FindComandasData>(url)
   }
 }
